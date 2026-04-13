2bca585e224702a6bde3b0e4fb04f4a43d9cdf75
max-1232 <adamsmax0109@gmail.com>
2026-04-13 15:52:33 +0200

    Nieuwe versie

--- app.py
+++ app.py
@@ -6,19 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def index() -> str:
     """Homepage route"""
-    return render_template("index.html", title="Welkom")
-
-@app.route("/informatie")
-def info() -> str:
-    return '<h1>Dit hebben we jou te bieden:</h1><p>Piano, gitaar, drums, zang</p>'
-
-@app.route("/contact")
-def contact() -> str:
-    return "<h1>Neem contact op</h1><p>Email: info@session.nl</p>"
-
-@app.route("/cursist/<naam>")
-def cursist(naam: str) -> str:
-    return f"<h1>Dit is de pagina van {naam}</h1>"
+    return "<h1>Welkom bij de eerste flask site</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
